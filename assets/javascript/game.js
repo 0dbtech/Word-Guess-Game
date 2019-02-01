ac3c1c9dffcec9a057adcc4add515a2997f2a56a
admin <admin@admins-MacBook-Pro.local>
2019-02-01 07:14:05 -0500

    more

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,8 +2,8 @@ var wordBank = ["avid", "adobe", "vegas", "protools", "harry"]
 
 var wins = 0;
 var losses = 0;
-var guess = 0;
-var wrong = 10;
+var wrongGuess = 0;
+var guessRemain = 10;
 var letters = [];
 var wrongLetters = [];
 
@@ -12,12 +12,12 @@ var wordSelect = wordBank[Math.floor(Math.random() * wordBank.length)];
 
 console.log("Available words are: " + wordBank);
 
-console.log("The secret work is " + wordSelect);
+console.log("The secret word is: " + wordSelect);
 
-console.log("testing index of with string conversion");
-x = "a";
-stringValue = String(x);
-console.log(wordSelect.indexOf(stringValue));
+// console.log("testing index of with string conversion");
+// x = "a";
+// stringValue = String(x);
+// console.log(wordSelect.indexOf(stringValue));
 
 for(i=0; i<wordSelect.length; i++){
 letters.push("_");
@@ -37,9 +37,15 @@ console.log("items in array letters " + letters);
 
 
 
-
+console.log("OUTSIDE OF DOC KEYUP");
 
 document.onkeyup = function(event) {
+  console.log("TOP OF DOC KEYUP");
+  console.log("guess remaining " + guessRemain);
+if (guessRemain === 0){
+  console.log("GAME OVER");
+  console.log("guess remaining " + guessRemain);
+}
 
 
   // Determines which key was pressed.
@@ -47,39 +53,99 @@ document.onkeyup = function(event) {
 
   console.log("about to run string conversion");
   stringValue = String(userGuess);
+
+  
+  console.log("guess remaining " + guessRemain);
   console.log(wordSelect.indexOf(stringValue));
 
 console.log("You just typed letter " + userGuess);
 console.log("reminder the word is " + wordSelect);
+console.log("guess remaining " + guessRemain);
+if ( (wordSelect.indexOf(stringValue)) === -1 ) {
+  console.log("WRONG");
+  console.log("guess remaining " + guessRemain);
+  // --guessRemain;
+  console.log("guess remaining " + guessRemain);
+  }
+  else {
+    console.log("CORRECT");
+    console.log("guess remaining " + guessRemain);
+  }
+
 
 for (i=0; i<wordSelect.length; i++){
+
+  //END GAME IF ALL LETTERS GUESSED
+  if ( (wordSelect.indexOf[i]) === (letters.indexOf[i]) ){
+  console.log("YOU WON");
+  }
   
   if (i === wordSelect.indexOf(stringValue) ){
   console.log(stringValue + " is a correct letter.");
   letters.splice(i, 1, stringValue);
+  console.log("guess remaining " + guessRemain);
   }
-  
-  
 
+  
+  
+  
+  // console.log("items in CORRECT array letters " + letters);
+
+  
+  
+  // else {
+  //   console.log(stringValue + " FIRST incorrect guess, no guesses lost FALSE = " + wrongLetters.includes(stringValue));
+    
+  // }
+  
+  
+  console.log("wrong letters " + wrongLetters);
 
 }
 
-console.log("items in CORRECT array letters " + letters);
-
-if (wordSelect.indexOf(stringValue) === -1) {
-  console.log(stringValue + " is a wrong letter.");
+if (wrongLetters.indexOf(stringValue) === -1){
+  console.log("guess remaining " + guessRemain);
+  console.log(stringValue + " was NOT already typed wrong");
   wrongLetters.splice(0, 0, stringValue);
-    console.log("wrong letters " + wrongLetters);
- }
-
-
-
-
-
+  // guessRemain--;
 }
+console.log("guess remaining " + guessRemain);
+  console.log("items in CORRECT array letters " + letters);
 
-  // incorrectText.push(event.key);
-  // wrongLettersText.textContent = "You have typed incorrectly: " + incorrectText;
+
+
+console.log ("done here");
+return
+//END
+}
+//END
+
+
+// if (wrongLetters.includes(stringValue) == false);{
+//   console.log(stringValue + " was already guessed incorrectly");
+// }
+
+
+// if (wordSelect.indexOf(stringValue) === -1) {
+//   console.log(stringValue + " adding to wrong array");
+//   wrongLetters.splice(0, 0, stringValue);
+//     console.log("wrong letters " + wrongLetters);
+//  }
+/* <h2><span id="wins-text"></span></h2>
+<h2><span id="guess-text"></span></h2>
+<h2><span id="wrong-text"></span></h2>
+<h2><span id="word-text"></span></h2> */
+
+
+ 
+
+
+
+
+
+
+
+
   
 
 
